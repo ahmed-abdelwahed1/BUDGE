4a6ccf738fe5b1774af20aec321ee9a976ca9a2d
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2025-12-11 17:43:58 +0200

    create categories screen

---
+++ frmCategories.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCategories
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        txtCatName = New TextBox()
+        rbIncome = New RadioButton()
+        rbExpense = New RadioButton()
+        btnSave = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtCatName
+        ' 
+        txtCatName.Location = New Point(90, 44)
+        txtCatName.Name = "txtCatName"
+        txtCatName.Size = New Size(125, 27)
+        txtCatName.TabIndex = 0
+        ' 
+        ' rbIncome
+        ' 
+        rbIncome.AutoSize = True
+        rbIncome.Location = New Point(90, 91)
+        rbIncome.Name = "rbIncome"
+        rbIncome.Size = New Size(121, 24)
+        rbIncome.TabIndex = 1
+        rbIncome.TabStop = True
+        rbIncome.Text = "RadioButton1"
+        rbIncome.UseVisualStyleBackColor = True
+        ' 
+        ' rbExpense
+        ' 
+        rbExpense.AutoSize = True
+        rbExpense.Location = New Point(90, 121)
+        rbExpense.Name = "rbExpense"
+        rbExpense.Size = New Size(121, 24)
+        rbExpense.TabIndex = 2
+        rbExpense.TabStop = True
+        rbExpense.Text = "RadioButton1"
+        rbExpense.UseVisualStyleBackColor = True
+        ' 
+        ' btnSave
+        ' 
+        btnSave.Location = New Point(103, 207)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(94, 29)
+        btnSave.TabIndex = 3
+        btnSave.Text = "Button1"
+        btnSave.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(273, 207)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(300, 188)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' frmCategories
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(btnSave)
+        Controls.Add(rbExpense)
+        Controls.Add(rbIncome)
+        Controls.Add(txtCatName)
+        Name = "frmCategories"
+        Text = "frmCategories"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtCatName As TextBox
+    Friend WithEvents rbIncome As RadioButton
+    Friend WithEvents rbExpense As RadioButton
+    Friend WithEvents btnSave As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ frmCategories.vb
@@ -0,0 +1,23 @@
+﻿Public Class frmCategories
+    Private Sub LoadCategories()
+        DataGridView1.DataSource = GetData("SELECT * FROM Categories")
+    End Sub
+
+    Private Sub frmCategories_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadCategories()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If txtCatName.Text = "" Then Exit Sub
+
+        Dim cType As String = If(rbIncome.Checked, "Income", "Expense")
+        ' جملة الإدراج
+        Dim sql As String = "INSERT INTO Categories (CategoryName, CatType) VALUES ('" & txtCatName.Text & "', '" & CType & "')"
+
+    If ExecuteQuery(sql) Then
+            MsgBox("Category Added!")
+            LoadCategories()
+            txtCatName.Clear()
+        End If
+    End Sub
+End Class
